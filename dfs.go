749aa67ac7e2216e0863d03a9d8de7938b7847e9
Daniil <e4z133o1@protonmail.com>
2024-08-24 23:08:27 +0300

    Add negative number check

--- dfs.go
+++ dfs.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func main() {
 
 	const graphFilePath = "graph.txt"
-	const startNode = 2
+	const startNode = -1
 	const finishNode = 3
-	graph := readGraph(graphFilePath)
-	fmt.Println(graph.nodes)
-	var pathLength int = graph.findPathLengthDFS(2, 3)
-	fmt.Println()
-	fmt.Printf("Path length from %d to %d is %d hops\n", startNode, finishNode, pathLength)
+	var inputErr = errors.New("can't use with negative numbers")
+
+	if startNode > 0 && finishNode > 0 {
+		graph := readGraph(graphFilePath)
+		fmt.Println(graph.nodes)
+		var pathLength int = graph.findPathLengthDFS(2, 3)
+		fmt.Println()
+		fmt.Printf("Path length from %d to %d is %d hops\n", startNode, finishNode, pathLength)
+	} else {
+		panic(inputErr)
+	}
+
 }
